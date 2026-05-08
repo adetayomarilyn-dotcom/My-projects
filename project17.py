2a2e22394a6f70de334c9848cb0b8a8f5ab763b5
adetayomarilyn-dotcom <adetayomarilyn@gmail.com>
2026-05-08 19:08:23 +0100

    ok

---
+++ project17.py
@@ -0,0 +1,11 @@
+tup1 = (4, 3, 2, 2, -1, 18)
+tup2 = (2, 4, 8, 8, 3, 2, 9)
+product1 = 1
+for i in tup1:
+    product1 = product1 * i
+
+product2 = 1
+for i in tup2:
+    product2 = product2 * i
+print("The product of tup1 is", product1)
+print("The product of tup2 is", product2)

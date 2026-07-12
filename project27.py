10fd00b9efb5420d478632f18bbad9c9d3594147
adetayomarilyn-dotcom <adetayomarilyn@gmail.com>
2026-07-12 19:52:04 +0100

    Create project27.py
    ok

---
+++ project27.py
@@ -0,0 +1,16 @@
+class Reverse:
+    def __init__(self, s=""):
+        
+        self.s = s
+
+    def get_reversed(self):
+        
+        return self.s[::-1]
+
+
+user_input = input("Enter a word to reverse: ")
+
+reverser = Reverse(user_input)
+
+reversed_word = reverser.get_reversed()
+print(f"Reversed word: {reversed_word}")
